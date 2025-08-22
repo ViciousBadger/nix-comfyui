b4b689db21206b50135d16cbfba987fe5460cdaf
badgerson <mail@badgerson.com>
2025-08-22 15:40:03 +0200

    also fails

--- environment/rocm/pyproject.nix
+++ environment/rocm/pyproject.nix
@@ -6,27 +6,27 @@ basePyproject.override {
   content = {
     tool.poetry.dependencies = {
       pytorch-triton-rocm = {
-        version = "~3.3.1";
+        version = "~3.4.0";
         source = "torch-rocm";
       };
       taichi = "*";
       torch = {
-        version = "~2.7.1+rocm6.3";
+        version = "~2.8.0+rocm6.4";
         source = "torch-rocm";
       };
       torchaudio = {
-        version = "~2.7.1";
+        version = "~2.8.0";
         source = "torch-rocm";
       };
       torchvision = {
-        version = "~0.22.1";
+        version = "~0.23.0";
         source = "torch-rocm";
       };
     };
 
     tool.poetry.source = lib.singleton {
       name = "torch-rocm";
-      url = "https://download.pytorch.org/whl/rocm6.3";
+      url = "https://download.pytorch.org/whl/rocm6.4";
       priority = "explicit";
     };
   };
